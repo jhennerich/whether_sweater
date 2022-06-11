16575edfa59eb673504ad6b509a21d534edb65cf
John Hennerich <john.hennerich@gmail.com>
2022-06-11 16:41:03 -0600

    Test: update test name

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Forecast API' do
-  it 'Finds and returns weather for a city', :vcr do
+  it 'Finds and returns weather data for a city', :vcr do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
