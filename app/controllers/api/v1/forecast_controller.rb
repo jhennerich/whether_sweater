4ec3b0a126fa81de99ce7c008a3bdb56fade78ae
John Hennerich <john.hennerich@gmail.com>
2022-06-12 09:19:36 -0600

    Feat: add param error checks to endpoints

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
+  before_action :check_params
+  
   def index
     forecast = ForecastFacade.forecast_by_coords(params[:location])
     render json: ForecastSerializer.new(forecast)
