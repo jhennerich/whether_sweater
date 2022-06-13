8da8c1b2f81d668dcb1c79542ea0ac61897af665
John Hennerich <john.hennerich@gmail.com>
2022-06-13 15:33:15 -0600

    Fix: update serializer to accept total_books_found

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -1,5 +1,5 @@
 class BooksSerializer
   include FastJsonapi::ObjectSerializer
   set_id { nil }
-  attributes :current_weather, :book
+  attributes :current_weather, :total_books_found, :books
 end
