f5c60b5727a177d4521caf91bf227d81be3894d1
John Hennerich <john.hennerich@gmail.com>
2022-06-13 09:59:19 -0600

    Test: build out openlib service spec test

--- spec/services/openlibrary_service_spec.rb
+++ spec/services/openlibrary_service_spec.rb
@@ -5,11 +5,17 @@ describe OpenlibService do
     it 'can get the get data for a book by location name', :vcr do
 
       response = OpenlibService.get_book_info('denver,co')
-      binding.pry
+
       expect(response).to be_a(Hash)
       expect(response).to have_key(:numFound)
-      expect(response[:numFound]).to be_a(Interger)
-      expect(response[:docs]).to be_an[Array]
+      expect(response[:numFound]).to be_a(Integer)
+      expect(response[:docs]).to be_a(Array)
+      expect(response[:docs][0]).to be_a(Hash)
+      expect(response[:docs][0]).to have_key(:isbn)
+      expect(response[:docs][0][:isbn]).to be_a(Array)
+      expect(response[:docs][0]).to have_key(:title)
+      expect(response[:docs][0][:title]).to be_a(String)
+
 
     end
   end
