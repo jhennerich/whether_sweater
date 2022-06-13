aac547ed1f77ded184cd00052383f2a2a8200977
John Hennerich <john.hennerich@gmail.com>
2022-06-13 15:30:18 -0600

    Fix: update book.rb to remove one level of book hash

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,15 +1,11 @@
 class Book
-  attr_reader :books
+  attr_reader :isbn,
+              :title,
+              :publisher
 
   def initialize(data)
-    @books = book_hash(data)
-  end
-
-  def book_hash(data)
-    {
-      isbn: data[:isbn],
-      title: data[:title],
-      publisher: data[:publisher]
-    }
+        @isbn = data[:isbn]
+        @title = data[:title]
+        @publisher = data[:publisher]
   end
 end
