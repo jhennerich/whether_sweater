a011b5a4a6b72b9905fa54481035fc64cc5ea709
John Hennerich <john.hennerich@gmail.com>
2022-06-13 15:32:31 -0600

    Fix: update forecast_book to include total_books_found

--- app/poros/forecast_book.rb
+++ app/poros/forecast_book.rb
@@ -1,11 +1,11 @@
 class ForecastBook
   attr_reader :current_weather,
               :total_books_found,
-              :book
+              :books
 
-  def initialize(current_weather, book, total_books_found)
+  def initialize(current_weather, total_books_found, books)
     @current_weather = current_weather
-    @book = book
+    @books = books
     @total_books_found = total_books_found
   end
 end
