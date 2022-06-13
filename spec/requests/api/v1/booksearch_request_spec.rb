6c9929c08897462e26102ad13dce9d3736e4d74f
John Hennerich <john.hennerich@gmail.com>
2022-06-13 09:31:05 -0600

    Test: begin endpoint spec

---
+++ spec/requests/api/v1/booksearch_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Book-Search API' do
+  describe 'Happy path' do
+
+    it 'Finds and returns book data for a city', :vcr do
+      get '/api/v1/book-search?location=denver,co&quantity=5'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to be_a(Hash)
+    end
+  end
+end
