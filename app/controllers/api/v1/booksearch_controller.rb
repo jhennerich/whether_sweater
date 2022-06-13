8f7240336d45836015ee4a708a0fb2a00d6c2ed7
John Hennerich <john.hennerich@gmail.com>
2022-06-13 15:31:20 -0600

    Fix: refactor logic to live in facade

--- app/controllers/api/v1/booksearch_controller.rb
+++ app/controllers/api/v1/booksearch_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::BooksearchController < ApplicationController
-#  before_action :check_params
+  before_action :check_params
 
   def index
     return if check_params
 
-    forecast = ForecastFacade.forecast_by_coords(params[:location])
     books = OpenlibFacade.get_book_info(params[:location], params[:quantity])
-    combine = ForecastBook.new(forecast.current_weather,books[0],books[1])
-#    render json: BooksSerializer.new(forecast)
-    render json: BooksSerializer.new(combine)
-#    render json: BooksSerializer.new(forecast, books[0])
+    render json: BooksSerializer.new(books)
   end
 end
