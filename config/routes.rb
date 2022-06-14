40737f59c2fccc3844a88bab231d7185d0896f18
John Hennerich <john.hennerich@gmail.com>
2022-06-13 21:30:32 -0600

    Response: add route for sessions create

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get 'backgrounds', to: 'backgrounds#show'
 
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
