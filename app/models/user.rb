65c467647336b990ee66d56f91ad91958ace9a1b
John Hennerich <john.hennerich@gmail.com>
2022-06-13 19:29:58 -0600

    User model development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token :api_key
+
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :api_key, uniqueness: true, presence: true
 end
